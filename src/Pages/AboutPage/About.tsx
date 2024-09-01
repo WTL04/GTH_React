17244225912df88ff7f4dfbcc9cf4d1558d805c5
WTL04 <luuwilliam11@gmail.com>
2024-09-01 16:40:15 -0700

    About Page in progress

--- src/Pages/AboutPage/About.tsx
+++ src/Pages/AboutPage/About.tsx
@@ -1,5 +1,48 @@
+import "./About.css";
+import NavBar from "../../Components/NavBar";
+import BottomBar from "../../Components/BottomBar";
+
+import { Fragment } from "react/jsx-runtime";
+
 function About() {
-  return <h1>About</h1>;
+  return (
+    <Fragment>
+      <div className="navBar">
+        <NavBar />
+      </div>
+
+      <div className="hero-banner">
+        <img src="/images/img0resize.png" alt="banner" />
+      </div>
+
+      <section className="about-paragraphs">
+        <div className="who-we-are">
+          <h1>Who We Are</h1>
+          <p>
+            oshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeih
+          </p>
+        </div>
+
+        <div className="our-purpose">
+          <h1>Our Purpose</h1>
+          <p>
+            oshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeih
+          </p>
+        </div>
+
+        <div className="our-mission">
+          <h1>Our Mission</h1>
+          <p>
+            oshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeihfooshfoshfoehfwoeihfwohfwofhwofhwofhewofhowfhwohfowehfowhfowfhwofhwohfowhfwohfowhfwohfwofhwofhwohfwohfeiwofhewoihfiowfoiewhfwoihefowhfeowhfowhfowhefowiehfowihfowiefhwoihfowiehfwohfwoeih
+          </p>
+        </div>
+      </section>
+
+      <div className="bottombar">
+        <BottomBar />
+      </div>
+    </Fragment>
+  );
 }
 
 export default About;
