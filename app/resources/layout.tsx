864cae5b0f83278163873b2d418b90b52f4ad8cd
WTL04 <williamluu04@gmail.com>
2025-02-03 16:00:48 -0800

    formating files

---
+++ app/resources/layout.tsx
@@ -0,0 +1,11 @@
+export default function ResourceLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
